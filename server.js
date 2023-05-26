6eb23341929d9542ceaddb22905ebefe46007c56
ahmedhesi <shiikh63@gmail.com>
2023-05-26 16:17:20 -0500

    done

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
+
+require('dotenv').config();
+require('./config/database');
+require('./config/passport');
+
 
 var indexRouter = require('./routes/index');
-var app = express();
-// load the "secrets" in the .env file
-require('dotenv').config();
-//Connect to database
-require('./config/database')
 var usersRouter = require('./routes/users');
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Add this middleware BELOW passport middleware
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -40,7 +63,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
 
 module.exports = app;
